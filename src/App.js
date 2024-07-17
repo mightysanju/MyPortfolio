f2e60b84b0c9d8f3911b22ee1bc07a38219d610d
SANJU KUMAR <sanju.k2r@gmail.com>
2024-07-16 19:53:09 -0500

    final

--- src/App.js
+++ src/App.js
@@ -7,32 +7,19 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-<<<<<<< HEAD
 import { Chat } from "./components/Chat";
-=======
-
->>>>>>> origin/main
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      
      <NavBar />
      <Banner />
      <Chat />
-=======
      <NavBar />
      <Banner />
->>>>>>> origin/main
      <Skills />
      <Projects />
      <Contact />
-
-<<<<<<< HEAD
-     
-=======
->>>>>>> origin/main
     </div>
   );
 }
